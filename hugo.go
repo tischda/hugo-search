5b020147486a720f3c7f3bb329aea9aaeaa8dad6
Daniel TISCHER <dos.7182@gmail.com>
2016-09-08 23:44:31 +0200

    fix build according to hugo api changes

--- hugo.go
+++ hugo.go
@@ -5,78 +5,32 @@ import (
 
 	"path/filepath"
 
-	"github.com/spf13/hugo/helpers"
 	"github.com/spf13/hugo/hugolib"
 	"github.com/spf13/viper"
 )
 
 // returns all pages of the hugo site located at path
 func readSitePages(path string) hugolib.Pages {
-	initializeConfig(path)
-	site := &hugolib.Site{}
-	checkFatal(site.Process())
-	return site.Pages
-}
 
-// stripped down version of hugolib.InitializeConfig()
-func initializeConfig(path string) {
+	// warning: if you specify configFilename, it will not search path
+	checkFatal(hugolib.LoadGlobalConfig(path, ""))
+
+	// this would be done in InitializeConfig() that we're NOT calling
+	// because it does not allow us to specify the source (path)
 	dir, _ := filepath.Abs(path)
 	viper.Set("WorkingDir", dir)
 
-	viper.AddConfigPath(path)
-	checkFatal(viper.ReadInConfig())
-	viper.RegisterAlias("indexes", "taxonomies")
+	sites, err := hugolib.NewHugoSitesFromConfiguration()
 
-	loadDefaultSettings()
-}
+	if err != nil {
+		log.Println("FATAL: Error creating sites", err)
+	}
 
-// copy of commands.loadDefaultSettings() since not public anymore
-func loadDefaultSettings() {
-	viper.SetDefault("cleanDestinationDir", false)
-	viper.SetDefault("Watch", false)
-	viper.SetDefault("MetaDataFormat", "toml")
-	viper.SetDefault("DisableRSS", false)
-	viper.SetDefault("DisableSitemap", false)
-	viper.SetDefault("DisableRobotsTXT", false)
-	viper.SetDefault("ContentDir", "content")
-	viper.SetDefault("LayoutDir", "layouts")
-	viper.SetDefault("StaticDir", "static")
-	viper.SetDefault("ArchetypeDir", "archetypes")
-	viper.SetDefault("PublishDir", "public")
-	viper.SetDefault("DataDir", "data")
-	viper.SetDefault("ThemesDir", "themes")
-	viper.SetDefault("DefaultLayout", "post")
-	viper.SetDefault("BuildDrafts", false)
-	viper.SetDefault("BuildFuture", false)
-	viper.SetDefault("UglyURLs", false)
-	viper.SetDefault("Verbose", false)
-	viper.SetDefault("IgnoreCache", false)
-	viper.SetDefault("CanonifyURLs", false)
-	viper.SetDefault("RelativeURLs", false)
-	viper.SetDefault("RemovePathAccents", false)
-	viper.SetDefault("Taxonomies", map[string]string{"tag": "tags", "category": "categories"})
-	viper.SetDefault("Permalinks", make(hugolib.PermalinkOverrides, 0))
-	viper.SetDefault("Sitemap", hugolib.Sitemap{Priority: -1, Filename: "sitemap.xml"})
-	viper.SetDefault("DefaultExtension", "html")
-	viper.SetDefault("PygmentsStyle", "monokai")
-	viper.SetDefault("PygmentsUseClasses", false)
-	viper.SetDefault("PygmentsCodeFences", false)
-	viper.SetDefault("PygmentsOptions", "")
-	viper.SetDefault("DisableLiveReload", false)
-	viper.SetDefault("PluralizeListTitles", true)
-	viper.SetDefault("PreserveTaxonomyNames", false)
-	viper.SetDefault("ForceSyncStatic", false)
-	viper.SetDefault("FootnoteAnchorPrefix", "")
-	viper.SetDefault("FootnoteReturnLinkContents", "")
-	viper.SetDefault("NewContentEditor", "")
-	viper.SetDefault("Paginate", 10)
-	viper.SetDefault("PaginatePath", "page")
-	viper.SetDefault("Blackfriday", helpers.NewBlackfriday())
-	viper.SetDefault("RSSUri", "index.xml")
-	viper.SetDefault("SectionPagesMenu", "")
-	viper.SetDefault("DisablePathToLower", false)
-	viper.SetDefault("HasCJKLanguage", false)
-	viper.SetDefault("EnableEmoji", false)
+	if err := sites.Build(hugolib.BuildCfg{SkipRender: true}); err != nil {
+		log.Println("FATAL: Error Processing Source Content", err)
+	}
+
+	return sites.Pages()
 }
 
 // checks if a page has a title (which will appear in the search result)
